5a6aa121a583757ed4b4d5392f278c0b51815be6
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-04-08 19:58:42 -0500

    clase 08-04-2026

---
+++ modulo_1/python/04_input.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cual es su nombre?")
+print(f"hola, {nombre}")
+
+edad_str = input("Ingrese la edad")
+print(f"Tienes, {edad_str} años.")
+edad = int(edad_str)
+print(f"El doble de tu edad es: {edad*2} años.")
